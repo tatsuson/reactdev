8223414cacb16ca848b67fec3f6b0e79caec3a8c
西口達樹 <tatsuson@nishikoutatsujunoMacBook-Pro.local>
2024-07-30 23:27:45 +0900

    add

--- src/App.js
+++ src/App.js
@@ -9,12 +9,19 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
     const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     nextSquares[i] = 'X';
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
